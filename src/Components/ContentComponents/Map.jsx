1ac808e538450df71daa7b90ad8e7f832c420b70
Prashanta <prashanta927pandit@gmail.com>
2024-09-30 16:15:37 +1000

    add a search field

--- src/Components/ContentComponents/Map.jsx
+++ src/Components/ContentComponents/Map.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import SearchResult from './SearchResult';
 
 const Map = () => {
     return (
         <div className='bg-gray-100 h-screen w-screen flex items-center justify-center'>
+            <div className=' flex flex-col items-start fixed top-0 left-0 ml-52 mt-10'>
+               <SearchResult />
+            </div>
             <p className='text-black text-xl'>We will be adding map soon. Stay tuned !</p>
         </div>
     );
